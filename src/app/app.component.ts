d8d110aeae8c155f31b8dd15880fc56c1e8fa79a
engtsopotsa <entsopotsa@outlook.com>
2024-05-11 23:43:45 +0200

    routing  part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { HeaderComponent } from './core/header/header.component';
+import { CartComponent } from './cart/cart/cart.component';
+import { CheckoutComponent } from './cart/checkout/checkout.component';
+import { HomePageComponent } from './home/home-page/home-page.component';
+import { SearchComponent } from './home/search/search.component';
+import { MenuListComponent } from './menu/menu-list/menu-list.component';
+import { RestaurantDetailComponent } from './menu/restaurant-detail/restaurant-detail.component';
+import { PaymentComponent } from './payment/payment/payment.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SignInComponent,SignUpComponent,HeaderComponent,CartComponent,CheckoutComponent,
+    HomePageComponent,SearchComponent,MenuListComponent,RestaurantDetailComponent,
+    PaymentComponent,NotFoundComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'TT-102-Food-Ordering-App-v1';
 }
+
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { HomePageComponent } from './home/home-page/home-page.component';
+import { CartComponent } from './cart/cart/cart.component';
+import { CheckoutComponent } from './cart/checkout/checkout.component';
+import { MenuListComponent } from './menu/menu-list/menu-list.component';
+import { RestaurantDetailComponent } from './menu/restaurant-detail/restaurant-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:SignInComponent},
+    {path:'sign-up',component:SignUpComponent},
+    {path:'home',component:HomePageComponent},
+    {path:'cart',component:CartComponent},
+    {path:'checkout',component:CheckoutComponent},
+    {path:'menu',component:MenuListComponent},
+    {path:'restaurants',component:RestaurantDetailComponent},
+    {path:'payment',component:PaymentMethodChangeEvent}
+
+];
